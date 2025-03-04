661d4b576f4fa56091cdd3a53cfd581c08864eb4
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-03-04 12:00:40 +0000

    datatypes introduction

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //use all javascript code as a newer version
+//alert( 3+ 3) // we are using nodejs, not browser
+/*
+For documentation use MDN and ECMA
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+https://tc39.es/ecma262/
+*/
+
+let name = "Anuja"
+let age = 18
+let isLoggedIn = false
+
+/*
+types of datatypes in JS
+String = ""
+Number = 2^53
+Bigint = big numbers
+Boolean = true/false
+Undefined = value not define(assign)
+Null = standalone value
+Symbol = uniqueness
+Object =
+*/
+
+console.log(typeof name);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
